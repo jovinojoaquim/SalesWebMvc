d6475f1b1725b0834fcb34bd11834f83ec249ba7
Jovino Joaquim <jovinojoaquim@hotmail.com>
2019-06-22 14:32:41 -0300

    Simple Create Form

--- SalesWebMvc/Data/SeedingService.cs
+++ SalesWebMvc/Data/SeedingService.cs
@@ -27,10 +27,10 @@ namespace SalesWebMvc.Data
             Department d2 = new Department(2, "Eletronic");
 
             Seller s1 = new Seller(1, "Leonardo", "leo.simoes@hotmail.com", new DateTime(1996, 12, 3), 3000.0, d1);
-            Seller s2 = new Seller(1, "Alisson", "alissonrodrigues@hotmail.com", new DateTime(1996, 12, 3), 3000.0, d2);
+            Seller s2 = new Seller(2, "Alisson", "alissonrodrigues@hotmail.com", new DateTime(1996, 12, 3), 3000.0, d2);
 
             SalesRecord sr1 = new SalesRecord(1, new DateTime(2019, 6, 20), 2000.0, SalesStatus.Billed, s1);
-            SalesRecord sr2 = new SalesRecord(1, new DateTime(2019, 6, 20), 3000.0, SalesStatus.Billed, s2);
+            SalesRecord sr2 = new SalesRecord(2, new DateTime(2019, 6, 20), 3000.0, SalesStatus.Billed, s2);
 
             _context.Department.AddRange(d1, d2);
             _context.Seller.AddRange(s1, s2);
