30328eefa9a22ac78c48d644f063759eaafc1869
Jovino Joaquim <jovinojoaquim@hotmail.com>
2019-06-22 14:56:38 -0300

    ForeignKey not null

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -14,6 +14,7 @@ namespace SalesWebMvc.Models
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public ICollection<SalesRecord> Salles { get; set; } = new List<SalesRecord>();
+        public int DepartmentsId { get; set; }
 
         public Seller() { }
 
