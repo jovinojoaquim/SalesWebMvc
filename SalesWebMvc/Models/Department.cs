4584117fcad7935bb10f9f708944cbebfa20bfe3
Jovino Joaquim <jovinojoaquim@hotmail.com>
2019-07-29 21:30:11 -0300

    Implementing grouping search

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -24,7 +24,7 @@ namespace SalesWebMvc.Models
             Sellers.Add(seller);
         }
 
-        public double TotalSellers(DateTime initial, DateTime final)
+        public double TotalSalers(DateTime initial, DateTime final)
         {
             return Sellers.Sum(seller => seller.TotalSales(initial, final));
         }
